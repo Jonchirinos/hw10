357e68dc2a98ecdd41776727564e7dec04074e7b
Jon Chirinos <chirinosjon@gmail.com>
2022-02-26 14:56:07 -0600

    cleaned up classes

--- index.js
+++ index.js
@@ -3,70 +3,40 @@ const inquirer = require("inquirer");
 inquirer
     .prompt([
         {
-            name: "title",
-            type: "input",
-            message: "Project Title?",
+            name: "role",
+            type: "list",
+            choices: ["Manager", "Engineer", "Intern", "Finish"],
+            message: "What Job Role Would You Like To Select?",
         },
         {
-            name: "description",
+            name: "name",
             type: "input",
-            message: "Insert description of the project you have developed",
+            message: "Enter Employee Name",
         },
         {
-            name: "tests",
+            name: "id",
             type: "input",
-            message: "How do you test the app?",
+            message: "EnterEmployee ID?",
         },
         {
-            name: "contribution",
+            name: "email",
             type: "input",
-            message: "What were your contributions to the development of this app?",
+            message: "Enter Employee's Email Address?",
         },
         {
-            name: "installation",
+            name: "office",
             type: "input",
-            message: "How do you install the app?",
-        },
-        {
-            name: "usage",
-            type: "input",
-            message: "What should the user do?",
-        },
-        {
-            name: "screenshot",
-            type: "input",
-            message: "Link to image of completed app?",
-        },
-        {
-            name: "questions",
-            type: "input",
-            message: "Any questions concerning the app or its development?",
-        },
-        {
-            name: "pages",
-            type: "input",
-            message: "Enter GitHub Pages URL",
-        },
-        {
-            name: "repo",
-            type: "input",
-            message: "Enter GitHub Repo URL",
+            message: "Enter Office Number",
         },
         {
             name: "github",
             type: "input",
-            message: "Enter your GitHub Username",
+            message: "Enter The GitHub Username",
         },
         {
-            name: "contact",
+            name: "school",
             type: "input",
-            message: "Enter preferred contact email",
-        },
-        {
-            name: "license",
-            type: "list",
-            message: "Which license was used for the application?",
-            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+            message: "Enter Employee's School",
         },
     ])
     .then((answers) => {
