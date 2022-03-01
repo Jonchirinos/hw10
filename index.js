01e3c3ccea03a0fde2e978d347abc7485922e71a
Jon Chirinos <chirinosjon@gmail.com>
2022-02-28 21:53:58 -0600

    added switch statements

--- index.js
+++ index.js
@@ -4,60 +4,74 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const fs = require("fs");
+const employees = [];
 
-inquirer
-    .prompt([
-        {
-            name: "role",
-            type: "list",
-            choices: ["Manager", "Engineer", "Intern"],
-            message: "What Job Role Would You Like To Select?",
-        },
-        {
-            name: "name",
-            type: "input",
-            message: "Enter Employee Name",
-        },
-        {
-            name: "id",
-            type: "input",
-            message: "EnterEmployee ID?",
-        },
-        {
-            name: "email",
-            type: "input",
-            message: "Enter Employee's Email Address?",
-        },
-        {
-            name: "office",
-            type: "input",
-            message: "Enter Office Number",
-        },
-        {
-            name: "github",
-            type: "input",
-            message: "Enter The GitHub Username",
-        },
-        {
-            name: "school",
-            type: "input",
-            message: "Enter Employee's School",
-        },
-        {
-            name: "continue",
-            type: "list",
-            choices: ["yes", "no"],
-            message: "Would you like to add another employee?",
-        },
-    ])
-    .then((answers) => {
-        const data = generateIndex(answers);
-        console.log("data", data);
-        function writeToFile(fileName, data) {
-            fs.writeFile(fileName, data, (err) => (err ? console.error(err) : console.log("Finished")));
-            // fs.writeFile( file, data, options, callback )
+function promptUser() {
+    inquirer
+        .prompt([
+            {
+                name: "role",
+                type: "list",
+                choices: ["Manager", "Engineer", "Intern"],
+                message: "What Job Role Would You Like To Select?",
+            },
+            {
+                name: "name",
+                type: "input",
+                message: "Enter Employee Name",
+            },
+            {
+                name: "id",
+                type: "input",
+                message: "EnterEmployee ID?",
+            },
+            {
+                name: "email",
+                type: "input",
+                message: "Enter Employee's Email Address?",
+            },
+            {
+                name: "office",
+                type: "input",
+                message: "Enter Office Number",
+            },
+            {
+                name: "github",
+                type: "input",
+                message: "Enter The GitHub Username",
+            },
+            {
+                name: "school",
+                type: "input",
+                message: "Enter Employee's School",
+            },
+            {
+                name: "continue",
+                type: "list",
+                choices: ["yes", "no"],
+                message: "Would you like to add another employee?",
+            },
+        ])
+        .then((answers) => {
+            switch (answers.role) {
+                case "Intern":
+                    const newIntern = new Intern("a", "a", "a", "a");
+                    // console.log(newIntern);
+                    employees.push(newIntern);
+                    break;
+                case "Engineer":
+                    const newEngineer = new Engineer("a", "a", "a", "a");
+                    // console.log(newIntern);
+                    employees.push(newEngineer);
+                    break;
+                case "Manager":
+                    const newManager = new Manager("a", "a", "a", "a");
+                    // console.log(newIntern);
+                    employees.push(newManager);
+                    break;
+            }
 
-            // TODO: check answers.role
+            // console.log(employees);
             // if role is 'manager' create new manager object and push into employee array
             // if role is 'engineer' create new manager object and push into employee array
             // if role is 'intern' create new manager object and push into employee array
@@ -66,11 +80,38 @@ inquirer
             // IN NEW FUNCTION else create string literal. note if user doesn't continue, generate html function
             // inside of html loop through employees array to generate divs for cards
             // write employees to html file
+
+            writeToFile("index.html", htmlTemplate(employees));
+        });
+
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => (err ? console.error(err) : console.log("Finished")));
+        // fs.writeFile( file, data, options, callback )
+
+        // TODO: check answers.role
+    }
+}
+
+function htmlTemplate(data) {
+    let tempData = "";
+
+    for (let i = 0; i < data.length; i++) {
+        let specialField = "";
+
+        if (data[i].role === "Intern") {
         }
-        writeToFile("README.md", data);
-    });
-// TODO: Generate HTML
-`<!DOCTYPE html>
+        let cardTemplate = `
+        <div>
+        <h1>Name: ${data[i].name}</h1>
+        <p>Id: ${data[i].name} </p>
+        <p>${specialField} </p>
+        </div>
+    `;
+
+        tempData = tempData + cardTemplate;
+    }
+
+    return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -79,18 +120,11 @@ inquirer
         <title>Document</title>
     </head>
     <body>
-        
-    </body>
-    </html>` // TODO: Generate card //
-`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        
+
+    ${tempData}
+
     </body>
     </html>`;
+}
+
+// TODO: Generate HTML
